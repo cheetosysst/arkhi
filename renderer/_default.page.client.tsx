52482d9b64667d175c2ffe8cb6ba848fc7343417
cheetosysst <thect@protonmail.com>
2023-06-25 12:37:56 +0800

    fix: remove unused mock functions

--- renderer/_default.page.client.tsx
+++ renderer/_default.page.client.tsx
@@ -1,16 +1,4 @@
-import type { FunctionComponent } from "react";
 import { renderIslands } from "@arkhi/core";
-
-// Imported for mock purpose
-import Counter from "../pages/index/Counter";
-import Accumulator from "../pages/index/Accumulator";
-
-const islandsComponents = new Map<string, FunctionComponent>();
-
-const mockPluginBehaviour = () => {
-	islandsComponents.set("Counter", Counter);
-	islandsComponents.set("Accumulator", Accumulator);
-};
 
 function render() {
 	renderIslands(document.body);
