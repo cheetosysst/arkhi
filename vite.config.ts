1e41c8bd4df561100ccdbef120feee66abf4f061
cheetosysst <thect@protonmail.com>
2023-07-10 16:28:28 +0800

    fix: change plugin import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
-import { arkhiCMS } from "./arkhi/plugins/cms";
+import { arkhiCMS } from "./arkhi/plugins";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { UserConfig } from "vite";
 
