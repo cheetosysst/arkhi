21f41d300de8ace03e53847f95bdea53e58dec59
Nehcled <del36031937@gmail.com>
2023-07-20 15:18:54 +0800

    init tailwind

--- renderer/PageShell.tsx
+++ renderer/PageShell.tsx
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import { PageContextProvider } from "./usePageContext";
 import type { PageContext } from "./types";
 import "./PageShell.css";
+import "./index.css"
 import { Link } from "@/arkhi/client";
 
 export { PageShell };
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './arkhi/**/*.{html, js, ts, jsx, tsx}',
+    './pages/**/*.{html, js, ts, jsx, tsx}',
+    './**/*.{html, js, ts, jsx, tsx}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
