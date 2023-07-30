0339df6269df0f45b447c684639022cada7346a0
amyD0947639 <homeamy29@gmail.com>
2023-07-30 15:00:11 +0800

    refactoring clean-code-plugin
    1.plugin now only runs at build time
    2. handle dynamic imports ensures that the plugin can accurately trace modules that are dynamically imported
    3. use  TypeScript AST (Abstract Syntax Tree) to analyze module imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import react from "@vitejs/plugin-react";
 import ssr from "vite-plugin-ssr/plugin";
 import { arkhiCMS } from "./arkhi/plugins";
+import { CleanUnusedExportsPlugin } from "./arkhi/plugins";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { UserConfig } from "vite";
-import CleanUnusedExportsPlugin from "./clean-code";
+
 const config: UserConfig = {
-	plugins: [react(), ssr(), tsconfigPaths(), arkhiCMS(),
-	CleanUnusedExportsPlugin('index.page'),
+	plugins: [react(), ssr(), tsconfigPaths(), arkhiCMS(), CleanUnusedExportsPlugin(),
 	],
 };
 
