f5b73c3e3f7293bf77ceb8cf13d6d5f80d4b8f06
cheetosysst <thect@protonmail.com>
2023-08-23 16:42:06 +0800

    fix: base url for trpc proxy

--- arkhi/client/api.ts
+++ arkhi/client/api.ts
@@ -2,12 +2,24 @@ import type { AppRouters } from "@/api";
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 
 /**
+ * Checks current enviroment and returns current base URL.
+ * Copied from T3.
+ * @returns Base URL
+ */
+const getBaseUrl = () => {
+	if (typeof window !== "undefined") return "";
+	// TODO add more edge runtime options.
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+	return `http://localhost:${process.env.PORT ?? 3000}`;
+};
+
+/**
  * Info: https://trpc.io/docs/client/nextjs/server-side-helpers#2-external-router
  */
 export const api = createTRPCProxyClient<AppRouters>({
 	links: [
 		httpBatchLink({
-			url: `http://localhost:3000/trpc`,
+			url: `${getBaseUrl()}/trpc`,
 		}),
 	],
 });
