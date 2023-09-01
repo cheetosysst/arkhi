dbe79cecc6189d6d7b3f8efe0c3af6cf44e973a7
amyD0947639 <homeamy29@gmail.com>
2023-09-01 16:56:52 +0800

    add code comments

--- arkhi/plugins/cms.ts
+++ arkhi/plugins/cms.ts
@@ -1,11 +1,12 @@
 import { Plugin } from 'vite';
-import { PathLike, promises as fs } from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
 import { compile } from '@mdx-js/mdx';
 import matter from 'gray-matter';
 
-let contentDirectoryPath: string
+let contentDirectoryPath: string //儲存content目錄相對位置
 
+// 從dirPath指向的檔案夾下獲取所有具有ext擴展名的文檔，返回儲存所有文檔絕對路徑的array
 export async function getFilesInDir(dirPath: string, ext: string[]): Promise<string[]> {
 	const entries = await fs.readdir(dirPath, { withFileTypes: true });
 	const files = await Promise.all(entries.map(async (entry) => {
@@ -20,6 +21,7 @@ export async function getFilesInDir(dirPath: string, ext: string[]): Promise<str
 	return files.flat();
 }
 
+// 生成一個vitural module，該module將導入指定的所有Markdown文件，並將它們導出為一個array內容
 async function generateModuleContent(id: string, fileTypes: string[], virtualId: string) {
 	const actualPath = id.replace(virtualId, '');
 	let absolutePath = path.join(__dirname, actualPath);
@@ -29,28 +31,34 @@ async function generateModuleContent(id: string, fileTypes: string[], virtualId:
 		contentDirectoryPath = absolutePath;
 	}
 	const mdFiles = await getFilesInDir(absolutePath, fileTypes);
+	// 生成文件的import，將文件內容引入
 	const imports = mdFiles.map((filePath, index) => {
 		const relativePath = path.relative(path.dirname(id), filePath).replace(/\\/g, '/');
 		return `import * as File${index} from '${relativePath}';`;
 	}).join('\n');
+	// 文件內容以及文件metadata
 	const fileObjects = mdFiles.map((_, index) => {
 		return `{ 
             component: File${index}.default, 
             metadata: File${index}.metadata 
         }`;
 	}).join(', ');
-
-	const exports = `export const allFiles = [${fileObjects}];`;
+	const exports = `export const allFiles = [${fileObjects}];`;// 導出所有fileObject內容
 	return `${imports}\n${exports}`;
 }
 
-async function transformMarkdownContent(id: PathLike, code: any) {
+// 將Markdown內容轉換為JSX，並將相應文件的metadata添加到程式導出的內容裡
+async function transformMarkdownContent(id: string, code: string) {
 	const fileStats = await fs.stat(id);
-	const { data, content } = matter(code);
+	const { data, content } = matter(code);// 使用gray-matter庫解析Markdown文件的front matter和content
+	// content中的所有中文字處理
 	const chineseCharacters = content.match(/[\u4e00-\u9fa5]/g);
 	const chineseCharacterCount = chineseCharacters ? chineseCharacters.length : 0;
+
+	// 計算content中的字數和閱讀時間
 	const words = content.replace(/\W+/g, '').length;
 	const readTime = (words + chineseCharacterCount) * 0.25;
+
 	const metadata = {
 		filePath: id,
 		fileName: path.basename(id as string),
@@ -58,18 +66,20 @@ async function transformMarkdownContent(id: PathLike, code: any) {
 		author: data.author,
 		tags: data.tags ? data.tags.split(',').map((tag: string) => tag.trim()) : [],
 		description: data.description,
-		readTime: readTime,
+		readTime: readTime, // 文件的閱讀時間，單位為秒
 		status: data.status,
-		atime: fileStats.atime,
-		mtime: fileStats.mtime,
-		ctime: fileStats.ctime,
-		createdAt: fileStats.birthtime,
+		atime: fileStats.atime, // 文件的上次訪問時間
+		mtime: fileStats.mtime, // 文件的上次修改時間
+		ctime: fileStats.ctime, // 文件的創建時間(最後修改metadata時間)
+		createdAt: fileStats.birthtime, // 文件本體的實際創建時間
 	};
+	// 使用@mdx-js/mdx將Markdown content轉為JSX 
+	// https://mdxjs.com/packages/mdx/#compilefile-options
 	const result = await compile(content, {
 		remarkPlugins: [],
 		rehypePlugins: [],
 		recmaPlugins: [],
-		mdExtensions: ['.md', '.markdown', '.mdown', '.mkdn', '.mkd', '.mdwn', '.mkdown', '.ron'],
+		mdExtensions: ['.md'],
 		mdxExtensions: ['.mdx'],
 		format: 'detect',
 		outputFormat: 'program',
@@ -77,13 +87,14 @@ async function transformMarkdownContent(id: PathLike, code: any) {
 		jsxImportSource: 'react',
 	});
 	const exportMetadata = `export const metadata = ${JSON.stringify(metadata)};`;
-	return result + '\n' + exportMetadata;
+	return result + '\n' + exportMetadata;// 返回轉換後的JSX和導出的metadata
 }
 
 export default function arkhiCMS(): Plugin {
 	return {
 		name: 'vite-mdx-plugin',
 		async resolveId(source) {
+			// 根據import的方式返回不同的resolvedId，import可以篩選所有來自md或mdx檔案、或找全部內容
 			if (source.endsWith('*.md')) {
 				const resolvedId = source.replace('*.md', '@virtual-md-files');
 				return resolvedId;
